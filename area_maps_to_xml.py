9e3ce02abf122ac953645f395e26f2976e2ecacb
Garrison Smith <44329882+GarrisonSmith@users.noreply.github.com>
2023-01-11 11:45:39 -0700

    Add files via upload

--- area_maps_to_xml.py
+++ area_maps_to_xml.py
@@ -9,23 +9,36 @@ tile_maps_path = os.path.join(directory_path, 'tile_maps')
 spritesheets_path = os.path.join(directory_path, 'spritesheets')
 tiles = list()
 
-def tile_image_is_in_tiles(tile_image): #determines if the provided tile_image is already in tiles or not
+#gets the locations list for the given layer of the given tile, if it does not currently exist then it creates and returns it.
+def get_layer_locations(tile, layer):
+	if tile[5] != [None]*2 :
+		for location_lists in tile[5]:
+			if location_lists[0] == layer:
+				return location_lists[1]
+	new_list = list()
+	tile[5].append([layer, new_list])
+	return new_list
+
+#determines if the provided tile_image is already in tiles or not
+def tile_image_is_in_tiles(tile_image):
 	for unqiue_tile in tiles:
 		diff = ImageChops.difference(unqiue_tile[0].convert('RGB'), tile_image.convert('RGB'))
 		if (diff.getbbox() is None):
 			return unqiue_tile #if tile graphic is found then the tile is returned
 	return False #tile image is not in tiles
 
-def add_tile(tile): #adds the provided tile to tiles if tile is a new graphic
+#adds the provided tile to tiles if tile is a new graphic
+def add_tile(tile):
 	if tile_image_is_in_tiles(tile[0]) is False:
 		tiles.append(tile)
 		return True #tile was added
 	return False #tile was not added
 			
-def get_spritesheet_tiles(): #clears then fills tiles
+#clears then fills tiles from the spritesheets folder
+def get_spritesheet_tiles():
 	tiles = list() #clears tiles
 
-	#checks for and adds the DEBUG graphics
+	#checks for and adds the DEBUG graphic, DEBUG also contains the empty textures.
 	spritesheet_path = os.path.join(spritesheets_path, 'DEBUG.png')
 	debug_image = Image.open(spritesheet_path)
 	if (debug_image.size[0] % tile_size != 0 or debug_image.size[1] % tile_size != 0):
@@ -33,33 +46,38 @@ def get_spritesheet_tiles(): #clears then fills tiles
 	for row in range(0, debug_image.size[1], tile_size):
 		for col in range(0, debug_image.size[0], tile_size):
 			tile_image = debug_image.crop([col, row, col+tile_size, row+tile_size]).convert('RGB')
-			tile = [tile_image, 'DEBUG', str(row//64) + ',' + str(col//64), row//64, col//64, list()]
+			tile = [tile_image, 'DEBUG', str(col//64) + ',' + str(row//64), col//64, row//64, list()]
 			add_tile(tile)
 
-	for spritesheet in os.listdir(spritesheets_path): #iterates through all spritesheets
-		if spritesheet == 'DEBUG': #DEBUG is already added
+	#iterates through all spritesheets
+	for spritesheet in os.listdir(spritesheets_path):
+		#DEBUG is already added
+		if spritesheet == 'DEBUG':
 			continue
-
 		spritesheet_path = os.path.join(spritesheets_path, spritesheet)
 		spritesheet_image = Image.open(spritesheet_path)
+		#spritesheet does not have valid dimensions that are evenly divisible by tile dimensions
 		if (spritesheet_image.size[0] % tile_size != 0 and spritesheet_image.size[1] % tile_size != 0):
 			print('Error in layer dimensions')
 			continue
-		for row in range(0, spritesheet_image.size[1], tile_size): #iterates through each tile in a spritesheet
+		#iterates through each tile in a spritesheet
+		for row in range(0, spritesheet_image.size[1], tile_size):
 			for col in range(0, spritesheet_image.size[0], tile_size):
 				tile_image = spritesheet_image.crop((col, row, col+tile_size, row+tile_size)).convert('RGB')
-				tile = [tile_image, spritesheet[0:-4], str(row//64) + ',' + str(col//64), row//64, col//64, list()]
+				tile = [tile_image, spritesheet[0:-4], str(col//64) + ',' + str(row//64), col//64, row//64, list()]
 				add_tile(tile)	
 
-def get_tile_xmls(map_root, layer_xml): #generates the tile XMLs for the current layer	
+#generates the tile XMLs for the current layer.	
+def get_tile_xmls(map_root, map_xml): 
 	for tile in tiles:
-		if (tile[5] == []): #tile has no locations on this layer
+		#tile has no locations on this layer
+		if (tile[5] == []): 
 			continue
 		
 		#generates the primary 'Tile' tag
 		tile_xml = map_root.createElement('Engine.Logic.Mapping.Tiling.Tile')
 		tile_xml.setAttribute('id', tile[1] + '|' + tile[2])
-		layer_xml.appendChild(tile_xml)
+		map_xml.appendChild(tile_xml)
 
 		#generates the 'tileSet' tag
 		tile_set = map_root.createElement('Spritesheet')
@@ -68,55 +86,61 @@ def get_tile_xmls(map_root, layer_xml): #generates the tile XMLs for the current
 	
 		#generates the 'tileSetCoordinates' tag
 		tile_set_coordinates = map_root.createElement('SheetCoordinates')
-		tile_set_coordinates.setAttribute('row', str(tile[3]))
-		tile_set_coordinates.setAttribute('col', str(tile[4]))
+		tile_set_coordinates.setAttribute('col', str(tile[3]))
+		tile_set_coordinates.setAttribute('row', str(tile[4]))
 		tile_xml.appendChild(tile_set_coordinates)
 
 		#generates the 'locations' tag
-		tile_locations = map_root.createElement('Locations')
-		for location in tile[5]:
-			tile_locations.appendChild(map_root.createTextNode(str(location)))
-		tile_xml.appendChild(tile_locations)
+		for layer_location in tile[5]:
+			tile_layer_locations = map_root.createElement('LayerLocations')
+			tile_layer_locations.setAttribute('layer', layer_location[0])
+			for location in layer_location[1]:
+				tile_layer_locations.appendChild(map_root.createTextNode(str(location)))
+			tile_xml.appendChild(tile_layer_locations)
 
-def get_layer_xml(layer_image, map_root, layer_xml): #generates the layer XML for the provided arguments
-	horizontal_length = layer_image.size[0]; vertical_length = layer_image.size[1]
-	
-	for tile in tiles: #clears tiles locations for new layer
-		tile[5] = list()
+#loads the location data for each tile in each layer in the current map.
+def load_tile_locations(layer_image, layer_number, map_layer_path):
 
-	if (horizontal_length % tile_size != 0 and vertical_length % tile_size != 0):
-		print('Error in layer dimensions')
+	if (layer_image.size[0] % tile_size != 0 and  layer_image.size[1] % tile_size != 0):
+		print('Error in layer dimensions: ' + map_layer_path)
+		print('Layer XML not generated.')
 		return
 	
-	for row in range(0, horizontal_length, tile_size):
-		for col in range(0, vertical_length, tile_size):
-			crop_area = (row, col, row+tile_size, col+tile_size)
+	for row in range(0, layer_image.size[0], tile_size):
+		for col in range(0,  layer_image.size[1], tile_size):
+			crop_area = (col, row, col+tile_size, row+tile_size)
 			tile_image = layer_image.crop(crop_area).convert('RGB')
 			tile = tile_image_is_in_tiles(tile_image)
 
-			if (tile is False): #tile was not found in a spritesheet and is a missing texture.
-				print('tile graphic not found in a spritesheet: ')
+			#tile was not found in a spritesheet and is a missing texture.
+			if (tile is False):
+				print('tile graphic not found in a spritesheet: ' + map_layer_path)
+				print('DEBUG tile placed instead.')
+				get_layer_locations(tiles[0], layer_number).append((col//64, row//64))
 				continue
 
-			if (tile[1] == 'DEBUG' and tile[4] != 0): #EMPTY tiles are not included in the XML.
+			#EMPTY tiles are not included in the XML.
+			if (tile[1] == 'DEBUG' and tile[3] != 0):
 				continue
 
-			tile[5].append((row//64, col//64))
-	
-	get_tile_xmls(map_root, layer_xml)
+			get_layer_locations(tile, layer_number).append((col//64, row//64))
 
-def get_map_xml(map_path, map_root, map_xml): #generates the map XML for the provided arguments
+#generates the map XML for the provided arguments
+def get_map_xml(map_path, map_root, map_xml): 
+	#clears tiles locations for new map
+	for tile in tiles:
+		tile[5] = list()
+	#iterates through layer images of map folder
 	for map_layer_name in os.listdir(map_path):
 		map_layer_path = os.path.join(map_path, map_layer_name)
 		layer_image = Image.open(map_layer_path)
-		
-		layer_xml = map_root.createElement('Engine.Logic.Mapping.MapLayer')
-		layer_xml.setAttribute('layer', map_layer_name[:-4])
-		map_xml.appendChild(layer_xml)
+		layer_number = map_layer_name[:-4]
+		load_tile_locations(layer_image, layer_number, map_layer_path)
 
-		get_layer_xml(layer_image, map_root, layer_xml)
+	get_tile_xmls(map_root, map_xml)
 
-def generate_maps_xml(): #generates the XML for all tile maps
+#generates the XML for all tile maps
+def generate_maps_xml():
 	for map in os.listdir(tile_maps_path):
 		map_path = os.path.join(tile_maps_path, map)
 		
